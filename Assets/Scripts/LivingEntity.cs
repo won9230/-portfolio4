c4c912b2bebd1fa4a8292bb4b46b48eade7f2ef5
won9230 <won9230@naver.com>
2022-04-30 17:11:40 +0900

    시작

---
+++ Assets/Scripts/LivingEntity.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingEntity : MonoBehaviour
+{
+	public float hp = 50f;
+	public float maxHp = 50f;
+	public float mp = 50f;
+	public float maxMp = 50f;
+	public bool dead = false;
+
+	public virtual void Hit(float damege)
+	{
+		hp -= damege;
+	}
+	public void DeadEntity()
+	{
+		if(hp < 0)
+		{
+			dead = true;
+		}
+	}
+}
